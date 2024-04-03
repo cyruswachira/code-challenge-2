12e89ed230ca959a98090b1bcd4d402f020ae794
CYRUS WACHIRA <mugowachira254@gmail.com>
2024-04-03 12:03:38 +0300

    add index-1.js

--- index-1.js
+++ index-1.js
@@ -2,7 +2,7 @@
 //Write a function that accepts a string as input and swaps the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
 
 //toUppercase()=>to uppercase characters
-//toLowerCase()=>
+//toLowerCase()=>to lowercase characters
 
 //Create a function that has a function name as swapCase and allows a paremeter string
 
